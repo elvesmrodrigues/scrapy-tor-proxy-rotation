06eaf147e4166ec60a4cfcb807e4d8ba2ec9c04a
Elves Rodrigues <elvesmateusrodrigues@outlook.com>
2020-06-03 21:04:33 -0300

    New setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="scrapy-tor-ip-rotator",
-    version="1.0.6",
+    version="1.0.7",
     author="Elves M. Rodrigues",
     author_email="elvesmateusrodrigues@gmail.com",
     description="Rotacionador de IP para o Scrapy via Tor",
@@ -14,9 +14,9 @@ setuptools.setup(
     url="https://github.com/elvesrodrigues/scrapy-tor-ip-rotator",
     packages=setuptools.find_packages(),
     install_requires=[
-        'requests',
-        'pysocks',
-        'stem',
+        'pysocks==1.7.1',
+        'requests==2.23.0',
+        'stem==1.8.0',
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
