cb443df7139947150e30301db216b3def8f0cb4f
Elves Rodrigues <elvesmateusrodrigues@outlook.com>
2023-03-08 10:03:25 -0300

    Update version info

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="scrapy-tor-proxy-rotation",
-    version="0.0.2",
+    version="0.0.3",
     author="Elves M. Rodrigues",
     author_email="elvesmateusrodrigues@gmail.com",
     description="IP Rotator for Scrapy via Tor",
